7edf87b25614e0d5541e9effb9cd8548a2bdc229
saveligulas <saveli.gulas@gmail.com>
2024-03-04 14:06:58 +0100

    new file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import lib.Location;
 import manager.RelationManager;
+import org.joda.time.LocalDate;
 import org.joda.time.Period;
 import people.Human;
 import physical.Book;
@@ -9,7 +10,7 @@ import physical.PhysicalObject;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human();
+        Human human = new Human("human", LocalDate.now());
         Location location = new Location("Location");
         Book book = new Book(human, location, "author", "book");
         book.simulateTimePassage(Period.years(1));
