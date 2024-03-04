7eb468ee57271edb6d006fc7feb5acd8cfff2342
saveligulas <saveli.gulas@gmail.com>
2024-03-04 14:05:15 +0100

    new file

---
+++ src/main/java/lib/furniture/LibraryServiceStation.java
@@ -0,0 +1,5 @@
+package lib.furniture;
+
+public interface LibraryServiceStation {
+
+}
---
+++ src/main/java/lib/furniture/Seatable.java
@@ -0,0 +1,8 @@
+package lib.furniture;
+
+import people.Human;
+
+public interface Seatable {
+    void sitPersonOnObject(Human human);
+    void getSeatAmount();
+}
