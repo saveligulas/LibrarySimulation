37a1609b7f3033a685cb4e318b66cfd685aaaa3b
saveligulas <saveli.gulas@gmail.com>
2024-03-05 16:39:18 +0100

    updates

--- src/main/java/lib/Location.java
+++ src/main/java/lib/Location.java
@@ -3,17 +3,28 @@ package lib;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.joda.time.LocalTime;
+import org.joda.time.Period;
 import physical.PhysicalObject;
+import simulation.TimeAffected;
+import simulation.TimeSimulator;
 
 import java.util.ArrayList;
 
 @Getter
 @AllArgsConstructor
-public class Location {
+public class Location implements TimeAffected {
     @Getter(AccessLevel.NONE)
     private static final ArrayList<String> UNIQUE_LOCATION_NAMES = new ArrayList<>();
     private final ArrayList<PhysicalObject> allObjects = new ArrayList<>();
     private String name;
+    private LocalTime clock;
+
+    public Location(TimeSimulator t, String name, LocalTime clock) {
+        this.name = name;
+        this.clock = clock;
+        assignToTimeSimulator(t);
+    }
 
     public void setName(String name) {
         if (UNIQUE_LOCATION_NAMES.contains(name)) {
@@ -27,5 +38,15 @@ public class Location {
             throw new IllegalArgumentException("Physical objects already exists at this location");
         }
         allObjects.add(physicalObject);
+    }
+
+    @Override
+    public void simulateTimeInObject(Period period) {
+        this.clock = clock.plus(period);
+    }
+
+    @Override
+    public void assignToTimeSimulator(TimeSimulator t) {
+        t.assignObject(this);
     }
 }
