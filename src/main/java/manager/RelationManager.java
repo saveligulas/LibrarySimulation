fbe746f952f9c178fcb956012e7995df6906e4f8
saveligulas <saveli.gulas@gmail.com>
2024-03-05 16:39:52 +0100

    updates

--- src/main/java/manager/RelationManager.java
+++ src/main/java/manager/RelationManager.java
@@ -1,15 +1,24 @@
 package manager;
 
+import exception.InvalidTimeException;
 import lib.Location;
+import lib.furniture.Counter;
+import org.joda.time.LocalTime;
+import people.LibraryWorker;
 import physical.PhysicalObject;
 
 public final class RelationManager {
-    public static void addLocationPhysicalObjectRelation(Location location, PhysicalObject physicalObject) {
+    public static void assignLibraryWorkerToCounter(LibraryWorker worker, Counter counter, LocalTime start, LocalTime end) {
+        if (start.compareTo(end) >= 0 || start.compareTo(counter.getOpeningTime()) < 0 || start.compareTo(counter.getClosingTime()) >= 0 ||
+            end.compareTo(counter.getOpeningTime()) <= 0 || end.compareTo(counter.getClosingTime()) > 0) {
+            throw new InvalidTimeException("Invalid time period");
+        }
         try {
-            location.addPhysicalObject(physicalObject);
-            physicalObject.setLocation(location);
-        } catch (IllegalArgumentException e) {
-            System.out.println("Operation failed: " + location + " with "+ physicalObject);
+            worker.setCounterStartTime(start);
+            worker.setCounterEndTime(end);
+            worker.setAssignedCounter(counter);
+        } catch (InvalidTimeException e) {
+            System.out.println("LibraryWorker could not be assigned");
         }
     }
 }
