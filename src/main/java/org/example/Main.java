d451ac2a937214fabc0dd6ef64a3c377dbfe422f
saveligulas <saveli.gulas@gmail.com>
2024-03-05 16:39:28 +0100

    updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,24 @@
 package org.example;
 
+import com.github.javafaker.Faker;
 import lib.Location;
+import lib.furniture.Counter;
 import manager.RelationManager;
-import org.joda.time.LocalDate;
-import org.joda.time.Period;
+import org.joda.time.*;
 import people.Human;
+import people.LibraryWorker;
 import physical.Book;
 import physical.PhysicalObject;
+import simulation.TimeData;
+import simulation.TimeSimulator;
 
 public class Main {
     public static void main(String[] args) {
-        Human human = new Human("human", LocalDate.now());
-        Location location = new Location("Location");
-        Book book = new Book(human, location, "author", "book");
-        book.simulateTimePassage(Period.years(1));
-        System.out.println(book.getLeaseTime().toString() + " " + book.getCurrentTime().toString());
+        TimeSimulator t = new TimeSimulator();
+        Human h = new Human(t, "human", LocalDate.now());
+        Location l = new Location(t, "location", TimeData.getCurrentDateTime().toLocalTime());
+        Book b = new Book(t, h, l, "author", "title");
+        t.simulateTimePassage(new Period(Hours.ONE));
+        System.out.println(t.getCurrentTime());
     }
 }
