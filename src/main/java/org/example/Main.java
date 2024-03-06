b914b3708fe57255023cdeee33ad1c223ef9c1de
saveligulas <saveli.gulas@gmail.com>
2024-03-06 11:27:25 +0100

    updates

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,8 @@ import com.github.javafaker.Faker;
 import lib.Location;
 import lib.furniture.Counter;
 import manager.RelationManager;
+import manager.TestClassRelationOne;
+import manager.TestClassRelationTwo;
 import org.joda.time.*;
 import people.Human;
 import people.LibraryWorker;
@@ -18,7 +20,11 @@ public class Main {
         Human h = new Human(t, "human", LocalDate.now());
         Location l = new Location(t, "location", TimeData.getCurrentDateTime().toLocalTime());
         Book b = new Book(t, h, l, "author", "title");
-        t.simulateTimePassage(new Period(Hours.ONE));
+        t.simulate(new Period(Years.ONE), 15);
         System.out.println(t.getCurrentTime());
+        TestClassRelationOne i = new TestClassRelationOne();
+        TestClassRelationTwo u = new TestClassRelationTwo();
+        RelationManager.setRelationship(i, u);
+        System.out.println(i.getRelationship() + " " + u.getRelationship());
     }
 }
