6548daa7515891c3b7e66c3c7fc40f421c6e233d
saveligulas <saveli.gulas@gmail.com>
2024-03-06 11:27:52 +0100

    updates

--- src/main/java/manager/RelationManager.java
+++ src/main/java/manager/RelationManager.java
@@ -1,11 +1,9 @@
 package manager;
 
 import exception.InvalidTimeException;
-import lib.Location;
 import lib.furniture.Counter;
 import org.joda.time.LocalTime;
 import people.LibraryWorker;
-import physical.PhysicalObject;
 
 public final class RelationManager {
     public static void assignLibraryWorkerToCounter(LibraryWorker worker, Counter counter, LocalTime start, LocalTime end) {
@@ -19,6 +17,16 @@ public final class RelationManager {
             worker.setAssignedCounter(counter);
         } catch (InvalidTimeException e) {
             System.out.println("LibraryWorker could not be assigned");
+        }
+    }
+
+    public static <T, U> void setRelationship(OneToOneRelationShip<U> relOne, OneToOneRelationShip<T> relTwo) {
+        try {
+            relOne.setRelationship(relOne.getRelationshipClass().cast(relTwo));
+            relTwo.setRelationship(relTwo.getRelationshipClass().cast(relOne));
+        } catch (Exception e) {
+            System.out.println("Relationship could not be assigned + " + relOne.toString() + " " + relTwo.toString());
+            e.printStackTrace();
         }
     }
 }
