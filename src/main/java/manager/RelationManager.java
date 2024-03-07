44f43d69ccdb072fc4b1e6caf290f00812e14181
saveligulas <saveli.gulas@gmail.com>
2024-03-07 17:40:32 +0100

    updates

--- src/main/java/manager/RelationManager.java
+++ src/main/java/manager/RelationManager.java
@@ -1,22 +1,16 @@
 package manager;
 
+import exception.DuplicateObjectException;
 import exception.InvalidTimeException;
 import lib.furniture.Counter;
 import org.joda.time.LocalTime;
 import people.LibraryWorker;
 
 public final class RelationManager {
-    public static void assignLibraryWorkerToCounter(LibraryWorker worker, Counter counter, LocalTime start, LocalTime end) {
+    public static void assignLibraryWorkerToCounter(LibraryWorker worker, Counter counter, LocalTime start, LocalTime end) throws InvalidTimeException {
         if (start.compareTo(end) >= 0 || start.compareTo(counter.getOpeningTime()) < 0 || start.compareTo(counter.getClosingTime()) >= 0 ||
             end.compareTo(counter.getOpeningTime()) <= 0 || end.compareTo(counter.getClosingTime()) > 0) {
             throw new InvalidTimeException("Invalid time period");
-        }
-        try {
-            worker.setCounterStartTime(start);
-            worker.setCounterEndTime(end);
-            worker.setAssignedCounter(counter);
-        } catch (InvalidTimeException e) {
-            System.out.println("LibraryWorker could not be assigned");
         }
     }
 
@@ -27,6 +21,19 @@ public final class RelationManager {
         } catch (Exception e) {
             System.out.println("Relationship could not be assigned + " + relOne.toString() + " " + relTwo.toString());
             e.printStackTrace();
+        }
+    }
+    
+    public static <T, U> void setRelationshipWithRelationClearing(OneToOneRelationShip<U> relOne, OneToOneRelationShip<T> relTwo) {
+        //TODO
+    }
+    
+    public static <T, U> void addRelationship(OneToManyRelationShip<U> oneToMany, OneToOneRelationShip<T> oneToOne) {
+        try {
+            oneToMany.addRelationship(oneToMany.getRelationshipClass().cast(oneToOne));
+            oneToOne.setRelationship(oneToOne.getRelationshipClass().cast(oneToMany));
+        } catch (DuplicateObjectException e) {
+            
         }
     }
 }
